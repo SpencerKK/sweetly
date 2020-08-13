420a33dc3cb3d7749fd877199e6981eed66f9458
SpencerKK <kenealys@me.com>
2020-08-13 14:50:59 -0700

    Hero Animation Completed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Hero.css";
 
 const Hero = () => {
-    const handleScroll = (e) => {
-        
+    const [scrollPos, setScrollPos] = useState(0);
+
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPos(position);
     }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    })
+
+    console.log(scrollPos);
+
     return (
         <div className="hero-wrapper">
-            <div className="lax-img"></div>
+            <div 
+                className="lax-img"
+                style={{ backgroundSize: `${80 + ( scrollPos / 5 ) }em` }}
+            >
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Hero;
+
+// Listen for screen position
+// set background size based on that position up to a point
